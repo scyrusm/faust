df9ee38b1581b5c07f5e79e2476f7cab4df965fd
Samuel Markson <smarkson@alum.mit.edu>
2022-09-21 16:33:47 -0400

    repository structuring

---
+++ setup.py
@@ -0,0 +1,36 @@
+#from distutils.core import setup
+from setuptools import setup, find_packages
+import os
+import io
+
+def _read(*parts, **kwargs):
+    filepath = os.path.join(os.path.dirname(__file__), *parts)
+    encoding = kwargs.pop('encoding', 'utf-8')
+    with io.open(filepath, encoding=encoding) as fh:
+        text = fh.read()
+    return text
+
+def get_requirements(path):
+    content = _read(path)
+    return [
+        req
+        for req in content.split("\n")
+        if req != '' and not req.startswith('#')
+    ]
+
+install_requires = get_requirements('requirements.txt')
+
+setup(
+    name='fits',
+    version='0.1dev',
+    author='Samuel C. Markson',
+    author_email='smarkson@alum.mit.edu',
+    install_requires=install_requires, 
+    packages=find_packages(),
+    scripts=['scripts/fits'],
+    license='BSD',
+    description='for CRISPR screen analysis',
+    include_package_data = True,
+    long_description=open('README.rst').read(),
+)
+
