d364cad331c709b8acb7f7f3e8681693abd9ff9c
Samuel Markson <smarkson@alum.mit.edu>
2022-09-22 17:07:45 -0400

    small changes

--- fits/visualization.py
+++ fits/visualization.py
@@ -1,5 +1,13 @@
-def plot_top_hits(summary_df, ascending=False,nhits=10,figsize=(9,5),fig=None, ax=None, 
-                  sort_criterion='CommonLanguageEffectSize',hue=None):
+def plot_top_hits(summary_df,
+                  ascending=False,
+                  nhits=10,
+                  figsize=(9, 5),
+                  fig=None,
+                  ax=None,
+                  sort_criterion='CommonLanguageEffectSize',
+                  hue=None,
+                  swarmviolin_kwargs={},
+                  selected_genes=None):
     """
 
     Parameters
@@ -29,19 +37,26 @@ def plot_top_hits(summary_df, ascending=False,nhits=10,figsize=(9,5),fig=None, a
     """
     import matplotlib.pyplot as plt
     from panopticon.visualization import swarmviolin
-    
+
     if fig is None and ax is None:
         fig, ax = plt.subplots(figsize=figsize)
     elif (fig is None and ax is not None) or (fig is not None and ax is None):
         raise Exception("either both or neither of fig, ax must be None")
     summary_df = summary_df.copy()
-    hits = summary_df.groupby('gene')[sort_criterion].mean().sort_values(ascending=False).head(nhits).index.values
-
-    swarmviolin(summary_df[summary_df['gene'].isin(hits)].sort_values(sort_criterion), y='CommonLanguageEffectSize',
-                x='gene',ax=ax,hue=hue,
-             #   annotate_hue_pvalue_fmt_str='p: {0:.2g}', pvalue='ttest',split=True,
-             #   custom_annotation_dict=gene2tumor_intersection_q, violinplot_kwargs={'color':'b'},
-                # swarmplot_kwargs={'color':'k'}
-
-           )    
+    if selected_genes is None:
+        hits = summary_df.groupby('gene')[sort_criterion].mean().sort_values(
+            ascending=False).head(nhits).index.values
+    else:
+        hits = selected_genes
+    swarmviolin(
+        summary_df[summary_df['gene'].isin(hits)].sort_values(sort_criterion),
+        y='CommonLanguageEffectSize',
+        x='gene',
+        ax=ax,
+        hue=hue,
+        **swarmviolin_kwargs
+        #   annotate_hue_pvalue_fmt_str='p: {0:.2g}', pvalue='ttest',split=True,
+        #   custom_annotation_dict=gene2tumor_intersection_q, violinplot_kwargs={'color':'b'},
+        # swarmplot_kwargs={'color':'k'}
+    )
     return fig, ax
